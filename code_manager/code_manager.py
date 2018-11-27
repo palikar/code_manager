3618ffef87d12fc39703907c689e9a23ee052fa5
Stanislav Arnaudov <stanislav_ts@abv.bg>
2018-11-27 23:56:17 +0100

    Restructuring the command line argument parser

--- code_manager/code_manager.py
+++ code_manager/code_manager.py
@@ -22,6 +22,12 @@ VERSION_MSG = [
 ]
 
 
+
+cache=None
+config=None
+usr_dir=None
+code_dir=None
+
 def get_arg_parser():
 
     parser = argparse.ArgumentParser(
@@ -31,24 +37,6 @@ def get_arg_parser():
     parser.add_argument('--version', '-v', action="version", version=('\n'.join(VERSION_MSG)),
                         help='Print veriosn inormation')
 
-    parser.add_argument('--setup-only', dest='setup', action="store_true", default=False,
-                        help='Only copy the config files if needed')
-
-    parser.add_argument('--list-packages', dest='list_pack', action="store_true", default=False,
-                        help='List the available packages in the packages.json file')
-
-    parser.add_argument('--clear-cache', dest='clear_cache', action="store_true", default=False,
-                        help='Clears the entries in the cach file')
-
-    parser.add_argument('--list-cache', dest='list_cache', action="store_true", default=False,
-                        help='Show the entries in the cache')
-
-    parser.add_argument('--install', dest='packages',
-                        help='Packages to install', nargs='+')
-
-    parser.add_argument('--reinstall', dest='reinstall',
-                        help='Packages to reinstall', nargs='+')
-
     parser.add_argument('--code-dir', dest='code_dir', action='store', required=False,
                         help='A folder to put the source of the packages')
 
@@ -56,24 +44,112 @@ def get_arg_parser():
                         help='A folder to install the packages')
 
     parser.add_argument('--packages-file', dest='packages_file', action='store',
-                        help='File to read the packages from')
+                        help='File to read the packages from', metavar='packages.json')
 
-    parser.add_argument('--install-all', dest='inst_all', action='store', type=int, default=None, nargs='?',
-                        const=-1, help='Install all packages in --packages from the given group')
 
-    parser.add_argument('--reinstall-all', dest='reall', action='store', type=int, default=None, nargs='?',
-                        const=-1, help='Reinstall all packages in --packages from the given group')
+    parser.add_argument('--setup-only', dest='setup', action="store_true", default=False,
+                        help='Only copy the config files if needed')
 
-    parser.add_argument('--no-install', dest='noinstall', action='store_true', default=False,
-                        help='If present, packages will only be downloaded')
+    
 
-    parser.add_argument('--focre--clear', dest='force_clear', action='store_true', default=False,
-                        help='If present, packages will only be downloaded')
+
+
+    
+
+    
+
+
+
+
+    subparsers = parser.add_subparsers(title='Commands', description='A list of avialble commands', dest='command', metavar='Command')
+
+    parser_install = subparsers.add_parser('install',description='Full installatio of packages', help='Installs packages (fetch, build and install)')
+    parser_install.add_argument('packages', nargs='?',  default=None, help='A list of packages to install')
+    parser_install.add_argument('--reinstall', dest='reinstall', action='store_true', help='Should the packages be reinstalled')
+    parser_install.add_argument('--group', action='store',metavar='name',default=None, help='Should the packages be reinstalled')
+
+    parser_fetch = subparsers.add_parser('fetch', description='Downloads packages but it does not install them nor build them', help='Downloads packages')
+    parser_fetch.add_argument('packages', nargs='?', default=None, help='A list of packages to fetch')
+    parser_fetch.add_argument('--focre--clear', dest='force_clear', action='store_true', default=False,
+                        help='Will delete any folders that stay on its way')
+    parser_fetch.add_argument('--group', action='store',metavar='name',default=None, help='If given, every package from this group will be fetched')
+
+    parser_build = subparsers.add_parser('build', description='Builds a package from source', help='Builds the project of package')
+    parser_build.add_argument('packages', nargs='?',  default=None, help='A list of packages to fetch')
+    parser_build.add_argument('--group', action='store', metavar='name',default=None, help='If given, every package from this group will be build')
+    parser_build.add_argument('--no-install', dest='noinstall', action='store_true', default=False,
+                        help='If present, packages will only be build but not installed')
+
+    parser_list_packages = subparsers.add_parser('list-packages', description='Lists the installed packages', help='Lists the installed packages')
+    parser_list_cach = subparsers.add_parser('list-cache', help='Show the entries in the cache')
+    parser_clear_cache = subparsers.add_parser('clear-cache', help='Clears the entries in the cach file')
+
 
     return parser
 
 
+
+
+def install(args,core):
+
+    if args.group is None:
+        core.install(args.packages, 
+                     reinstall=args.reinstall)
+    else:
+        core.install_group(args.group, 
+                     reinstall=args.reinstall)
+
+    
+
+def fetch(args,core):
+    pass
+
+def build(args,core):
+    pass
+
+def list_packages(args,core):
+    
+    print("Available packages:")
+    for pack in flatten(config['packages_list']):
+        print(pack)
+    
+
+
+def list_cache(args,core):
+
+    print(f"Dumping cache file {cache}")
+    f = open(cache, "r")
+    cont = f.read()
+    print(cont)
+    f.close()
+
+
+def clear_cache(args,core):
+
+    print(f"Clearing cache file {cache}")
+    f = open(cache, "w")
+    f.close()
+
+
+def get_commands_map():
+
+    commands = dict()
+
+    commands['install'] = install
+    commands['fetch'] = fetch
+    commands['build'] = build
+    commands['list-packages'] = list_packages
+    commands['list-cache'] = list_cache
+    commands['clear-cache'] = clear_cache
+
+    return commands
+
 def main():
+
+    global cache
+    global config
+    global usr_dir
+    global code_dir
 
     parser = get_arg_parser()
 
@@ -138,43 +214,23 @@ def main():
 
     with open(packages_file, "r") as config_file:
         config = json.load(config_file)
-
+        
     if args.setup:
         print("Setup for config files done. Exiting now! ")
         exit(0)
 
-    if args.list_pack:
-        print("Available packages:")
+    if args.command is None:
+        parser.print_help()
+        exit(1)
 
-        for pack in flatten(config['packages_list']):
-            print(pack)
+    commands = get_commands_map()
 
-        exit(0)
+    
+    core = Core(False, cache, config, code_dir, usr_dir,
+                install_scripts_dir, False)
+    
+    commands[args.command](args, core)
 
-    if args.clear_cache:
-        print(f"Clearing cache file {cache}")
-        f = open(cache, "w")
-        f.close()
-        exit(0)
-
-    if args.list_cache:
-        print(f"Dumping cache file {cache}")
-        f = open(cache, "r")
-        cont = f.read()
-        print(cont)
-        f.close()
-        exit(0)
-
-    core = Core(args.noinstall, cache, config, code_dir, usr_dir,
-                install_scripts_dir, args.force_clear)
-
-    if args.inst_all is not None:
-        core.install_all(config, code_dir, group=args.inst_all)
-    elif args.reall is not None:
-        core.install_all(config, code_dir, group=args.reall, reinstall=True)
-    else:
-        core.install(config, code_dir, args.packages)
-        core.install(config, code_dir, args.reinstall, reinstall=True)
 
 
 if __name__ == '__main__':
